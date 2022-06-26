8e959f6dc9ac4007e9b84ef6469c4c5d9b978d22
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-06-26 14:27:08 +0200

    added axios

--- src/components/apis/youtube.js
+++ src/components/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 const KEY = "AIzaSyD7op0R-a7Aayb6XLky5cN037Sfl-zaBvU";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
